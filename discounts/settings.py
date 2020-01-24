3c7642cecc72d682ea4217fb4f3592c18c9ba40a
liuchelx <liuchelx@gmail.com>
2020-01-23 16:51:21 -0800

    Update settings.py
    after merging version

--- discounts/settings.py
+++ discounts/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'discountsApp.apps.DiscountsappConfig',
-    'crispy_forms'
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
